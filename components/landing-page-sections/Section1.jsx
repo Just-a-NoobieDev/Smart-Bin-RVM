a5c7e5be63e8af68b02c4ffa9d8f5ebb9b63c0b2
Carl James Roxas <108602920+Just-a-NoobieDev@users.noreply.github.com>
2023-11-21 14:05:02 +0800

    Hot Fix v1.0.6

--- components/landing-page-sections/Section1.jsx
+++ components/landing-page-sections/Section1.jsx
@@ -2,13 +2,21 @@
 import { Button } from "../ui/Buttons";
 import { FaArrowRight } from "react-icons/fa";
 import Link from "next/link";
+import Image from "next/image";
+import machine from "@/public/images/machine.png";
 
 const Section1 = () => {
   return (
     <div>
       <div className="flex flex-col justify-center items-center text-center gap-5 lg:flex-row-reverse lg:items-center lg:justify-between">
         <div className="h-[50vh] bg-[#5B801A] flex items-center mt-[17vh] w-[70%] lg:w-[40%] justify-center lg:h-[100vh] lg:mt-0 ">
-          <div>MACHINE</div>
+          <div className="w-full h-full">
+            <Image
+              src={machine}
+              alt="machine"
+              className="w-full h-full object-contain"
+            />
+          </div>
         </div>
         <div className="m-5 text-[#5B801A] lg:text-start">
           <ul className="font-extrabold gap-5 text-3xl md:text-5xl lg:text-5xl max-w-6xl lg:gap-4 xl:text-6xl 2xl:text-7xl">
